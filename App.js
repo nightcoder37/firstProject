862c0273c37c5bd9f96dea51113f4df9c6c2bd72
Hassan-shah9 <hassan.thepearlsystem@gmail.com>
2024-12-15 17:18:59 +0700

    work on rn

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, Button} from 'react-native';
-import {CompanyData} from './components/CompanyData';
+// import {CompanyData} from './components/CompanyData';
 
 // const name = 'Ali';
 // let age = 20;
 // let email = 'abc@gmail.com';
 
 const App = () => {
-  let data = 100;
-  function fruit(val) {
-    data = 20;
-    console.warn(data);
-  }
+  const nameChange = () => {
+    setName('Ali Raza');
+  };
+  // let data = 100;
+  // function fruit(val) {
+  //   data = 20;
+  //   console.warn(data);
+  // }
+  const [name, setName] = useState('Ali');
 
   return (
     <View>
-      <Text style={{fontSize: 30}}>{data}</Text>
-      <Button title="On Press" onPress={() => fruit(40)} color={'green'} />
-      <Button title="On Press" onPress={fruit} color={'red'} />
+      {/* <Text style={{fontSize: 30}}>{data}</Text> */}
+      <Text style={{fontSize: 30}}>{name}</Text>
+      <Button title="Update Name" onPress={nameChange}></Button>
+      {/* <Button title="On Press" onPress={() => fruit(40)} color={'green'} />
+      <Button title="On Press" onPress={fruit} color={'red'} /> */}
     </View>
   );
 };
